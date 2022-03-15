a5847879d3f1eb1acbb6916bd297b0e9eb14f169
Admin <Admin@Dell>
2022-03-15 13:32:33 +0530

    Jenkins practice

---
+++ src/test/java/jenkins2/Jenkins2Test.java
@@ -0,0 +1,12 @@
+package jenkins2;
+
+import org.testng.annotations.Test;
+
+public class Jenkins2Test {
+
+	@Test
+	public void jenkins2() {
+		
+		System.out.println("this is from jenkins2");
+	}
+}
---
+++ src/test/java/jenkinspractice/Jenkins1Test.java
@@ -0,0 +1,12 @@
+package jenkinspractice;
+
+import org.testng.annotations.Test;
+
+public class Jenkins1Test {
+
+	@Test
+	public void jenkins1() {
+		
+		System.out.println("this is from jenkins1");
+	}
+}
